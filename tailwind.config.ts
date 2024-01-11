1f0d25ee32eff1ebdaa7c35d2c9e97c711ae016b
nikhil <nikhilshrestha0@gmail.com>
2024-01-11 06:27:44 -0600

    project-setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {},
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "hsl(28,98%,81%)",
+        secondary: "hsl(110, 100%, 88%)",
+        background: "hsl(56, 88%, 97%)",
+        accent: "hsl(249, 100%, 86%)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
